be362f9994b78fac4ace099c5fa3ec84337ca523
Raaz-009 <thameemraz829@gmail.com>
2024-06-20 20:10:22 +0530

    print No. using while

---
+++ while.cpp
@@ -0,0 +1,19 @@
+
+
+//code to print numbers using while loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n,i=1;
+
+cout<<"enter the value of n"<<endl;
+    cin>>n;
+
+while (i<=n){
+    cout<<i<<" ";
+    i=i+1;
+}
+}
